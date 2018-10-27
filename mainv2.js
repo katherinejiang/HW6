7b47d6991a714f6b77ddf9c99d06a7ae91a2d032
Katherine <katherinehjiang@gmail.com>
2018-10-26 23:39:56 -0400

    removing more h7, adding comments to js file

--- mainv2.js
+++ mainv2.js
@@ -3,11 +3,13 @@
 /*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 
 
+//this checks if the add to cart button is being clicked
 var elements = document.getElementsByClassName("button2 click");
 for (var i=0; i<elements.length; i++){
     elements[i].addEventListener("click", addToCart);
 }
 
+//goes through the form inputs and looks for which item is selected by the user
 function optionsVal (options){
     var selectedOption;
     for(var i=0; i<options.length;i++){
@@ -19,6 +21,7 @@ function optionsVal (options){
     return selectedOption;
 }
 
+//function that creates a cart item as an object
 function buildCartItem(id, name, glazing, price, quantity){
     var cartItem = {
     id:id,
@@ -32,16 +35,20 @@ function buildCartItem(id, name, glazing, price, quantity){
         return cartItem;
     }
 
+//this puts the cartItem into localStorage
 function saveShoppingCart(cartItem){
     cartFromStorage = JSON.parse(localStorage.getItem('shoppingCart')) || [];
     cartFromStorage.push(cartItem);
     console.log("hi i'm in the save");
-    //this puts into localStorage
+
     console.log(cartFromStorage);
     localStorage.setItem('shoppingCart', JSON.stringify(cartFromStorage));
 }
 
+//array where items are stored
 var cartFromStorage = [];
+
+//this runs whenever users adds items to their cart
 function addToCart(e){
     if (e && e.target){
         var quantityOptions;
@@ -54,21 +61,28 @@ function addToCart(e){
     var priceOptions = quantityOptions*5;
     var checkBunType = document.getElementById("bunName");
     var nameOptions = checkBunType.dataset.name;
+        
+    //creates a random id for each item in the cart
     var idNumber = Math.random();
     var shoppingCartItem = buildCartItem(idNumber, nameOptions, glazeOptions, priceOptions, quantityOptions);
     
+    //save to localStorage
     saveShoppingCart(shoppingCartItem);
+        
+    //update cart counter at the top of the page
     countItems();
         
     }
 }
-    
+
+//this clears the entire shopping cart and localStorage on the cart page when the button is clicked
 function clearShoppingCart(){
     console.log("shoppingcart cleared");
     localStorage.removeItem('shoppingCart');
     window.location.reload();
 }
-    
+
+//this updates the price when the quantity is changed on each product details page
 function updatePrice(){
     var quantityOptions = optionsVal(document.getElementsByName('quantity'));
     var whatToReplace = document.getElementById("changePrice").firstChild;
@@ -77,6 +91,7 @@ function updatePrice(){
     console.log(whatToReplace);
 }
 
+//this writes the shopping cart in HTML
 function populateCartHtml() {
     //get cart 
     var cart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
@@ -148,6 +163,7 @@ function populateCartHtml() {
         whatToReplace.nodeValue="$"+runningSubtotal;
 }
 
+//this counts the number of items in the cart and displays it next to Cart in the navbar
 function countItems(){
     var cart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
     console.log(cart);
@@ -163,9 +179,10 @@ function countItems(){
         }
 }
 
-var deleteButtonClicked = document.getElementsByClassName("removeButton");
-// deleteButtonClicked.addEventListener('click', deleteCartRow());
 
+var deleteButtonClicked = document.getElementsByClassName("removeButton");
+
+//this allows you to delete items in your cart
 function deleteCartRow(buttonNode){
     console.log(buttonNode);
     var deletedItemId = buttonNode.parentNode.id;
@@ -173,6 +190,7 @@ function deleteCartRow(buttonNode){
     buttonNode.parentNode.remove();
     
     var cart = JSON.parse(localStorage.getItem('shoppingCart'));
+    //goes through your cart array and removes the item with the matching id
     for(var i=0; i<cart.length; i++){
         if(cart[i].id == deletedItemId){
             cart.splice(i, 1);
